e4c23d842559eabccdc9be0e965e40d66be548db
August Saint Freytag <night@apricummedia.com>
2021-06-22 15:10:42 +0200

    Unify certificate parameter names and mapping to services
    Use uniform "_root" suffix for certificate paths.

--- scripts/compose.sh
+++ scripts/compose.sh
@@ -11,8 +11,8 @@ source "$DOCKERENV"
 set +a
 
 set +e
-mkdir "$SERVER_CERTIFICATE_DIRECTORY" &> /dev/null
-mkdir "$SERVER_CERTIFICATE_LOG_DIRECTORY" &> /dev/null
+mkdir "$SERVER_CERTIFICATE_ROOT" &> /dev/null
+mkdir "$SERVER_CERTIFICATE_LOG_ROOT" &> /dev/null
 set -e
 
 docker compose --file $DOCKERDIR/docker-compose.yml --project-directory $ROOTDIR $@	
