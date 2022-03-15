01babb71b331a685cabf75491fd4f8c28c283f5a
August Saint Freytag <night@apricummedia.com>
2022-03-15 12:25:58 +0100

    Run static content publish script with sh, add exit

--- scripts/publish-static-content.sh
+++ scripts/publish-static-content.sh
@@ -1,8 +1,8 @@
-#! /usr/bin/env bash
+#! /usr/bin/env sh
 
 # Static Content Publication Script
 
 BASEDIR=$(dirname "$0")
-cd "$BASEDIR/../server/ansible"
+cd "$BASEDIR/../server/ansible" || exit 1
 
 ansible-playbook playbook-set-up-services.yml --tags=app-static
